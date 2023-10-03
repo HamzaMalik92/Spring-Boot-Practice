463a161b142178889e7fe24e9e6ea72b59948ec0
Hamza Malik <hamzamalikcs20@gmail.com>
2023-10-03 15:21:36 +0500

    ic

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,16 +6,21 @@ import com.example.demo.game.MarioGame;
 import com.example.demo.game.SuperContraGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.demo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
 
-		GamingConsole game = new SuperContraGame();
-		GamingConsole game1 = new MarioGame();
-		GameRunner gameRunner =new GameRunner(game1);
+		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+//		GamingConsole game = new SuperContraGame();
+//		GamingConsole game1 = new MarioGame();
+//		GameRunner gameRunner =new GameRunner(game1);
+		GameRunner gameRunner = context.getBean(GameRunner.class);
 
 		gameRunner.run();
 
